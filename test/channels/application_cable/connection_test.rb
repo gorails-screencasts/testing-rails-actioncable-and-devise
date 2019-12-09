c2459351b0e496a7ccbf194e997092b991458281
Chris Oliver <excid3@gmail.com>
2019-12-09 13:43:57 -0600

    Add test for actioncable devise authentication

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -8,4 +8,30 @@ class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
   #
   #   assert_equal connection.user_id, "42"
   # end
+
+  test "connects with devise" do
+    user = users(:one)
+    connect_with_user(user)
+    assert_equal connection.current_user, user
+  end
+
+  test "unauthorized without devise" do
+    assert_raises ActionCable::Connection::Authorization::UnauthorizedError do
+      connect_with_user(nil)
+    end
+  end
+
+  private
+
+    def connect_with_user(user)
+      connect env: { 'warden' => FakeEnv.new(user) }
+    end
+
+    class FakeEnv
+      attr_reader :user
+
+      def initialize(user)
+        @user = user
+      end
+    end
 end
